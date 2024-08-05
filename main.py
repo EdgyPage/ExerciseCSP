a7b3d041ca40af43a2d0edf66cb084cee126546d
EdgyPage <myfirstbattle@gmail.com>
2024-08-04 21:21:11 -0500

    Changed filter to accomodate new training restrictions

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ csp = Constraint()
 csp.setAttributes(expandedList = [
                                   #(movement, num of times per cycle)
                                   (m['squat'], 1),
-                                  (m['leg press'], 1),
+                                  #(m['leg press'], 1),
                                   (m['stiff leg deadlift'], 1),
                                   (m['reverse hyper'], 1),
-                                  (m['glute ham raise'], 1),
+                                  #(m['glute ham raise'], 1),
                                   
                                   (m['bench press'], 1), 
                                   (m['overhead press'], 1),
-                                  (m['chest fly'], 1),
+                                  #(m['chest fly'], 1),
                                   (m['skullcrusher'], 1),
 
                                   (m['lat pulldown'], 1),
@@ -24,21 +24,21 @@ csp.setAttributes(expandedList = [
                                   (m['bicep curl'], 1),
                                   #(m['lateral raise'], 1),
                                   (m['shrug'], 1),
-                                  (m['reverse fly'], 2),
-                                  (m['ab wheel'], 2)
+                                  (m['reverse fly'], 1),
+                                  #(m['ab wheel'], 2)
                                   ],
-                    cycleLength = 5, #how many days long is your cycle 
-                    compoundLimit = 3, #max compound movements per day
+                    cycleLength = 4, #how many days long is your cycle 
+                    compoundLimit = 2, #max compound movements per day
                     isolationLimit = 3, #max isolation movements per day
-                    totalLimit = 4, #max total movements per day
-                    totalMin = 3, #min total movements per day
-                    compoundGap = 1, #amount of days between overlapping compound movements
-                    isolationGap = 1, #amount of days between overlapping isolation movements
-                    compoundMin = 1, #min amount of compound movements per session
-                    isolationMin = 1, #min amoun of isolation movements per session
+                    totalLimit = 6, #max total movements per day
+                    totalMin = 1, #min total movements per day
+                    compoundGap = 0, #amount of days between overlapping compound movements
+                    isolationGap = 0, #amount of days between overlapping isolation movements
+                    compoundMin = 0, #min amount of compound movements per session
+                    isolationMin = 0, #min amoun of isolation movements per session
                     #1 fatigue is roughly 1 minute in my current tuning
                     fatigueLimit = 75, #increase the fatigue maximum to make your programs potentially more exhausting 
-                    fatigueMin = 45 #lower the fatigue minimum to see WAY more options
+                    fatigueMin = 25 #lower the fatigue minimum to see WAY more options
 )
 
 answers = []
@@ -47,14 +47,21 @@ if assign.meetSpace():
     assign.findAssignment(assign.expandedList, answers)
     answers = list(set(answers))
     
-    for answer in answers:
-        print(answers.index(answer)+1)
-        print(answer)
+    #for answer in answers:
+    #    print(answers.index(answer)+1)
+    #    print(answer)
     
     #little code snippet below can be commented out
     #it's meant to create a sublist of all answers where a Movement is assigned to a particular day
     #complete implementation for this idea would require a slight edit to Assignments to take in partial Assignments but whatever
-    filtered = [answer for answer in answers if m['bench press'] in answer.progressSchedule[1] and m['squat'] in answer.progressSchedule[5] and m['shrug'] in answer.progressSchedule[1]]
+    filtered = [answer for answer in answers 
+                if m['squat'] in answer.progressSchedule[4] 
+                and m['bench press'] in answer.progressSchedule[1] 
+                and m['stiff leg deadlift'] in answer.progressSchedule[1]
+                and m['shrug'] in answer.progressSchedule[1]
+                and len(answer.progressSchedule[1]) == 3
+                and (len(answer.progressSchedule[4]) == 1 or (len(answer.progressSchedule[4]) == 2))
+                ]
     print('______________________________')
     for filt in filtered:
         print(filtered.index(filt)+1)
